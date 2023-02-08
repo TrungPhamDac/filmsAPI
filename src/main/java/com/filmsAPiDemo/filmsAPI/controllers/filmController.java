2f6444a937b2a3e1c34c8f07d277daf7c8546015
TrungPhamDac <84670001+TrungPhamDac@users.noreply.github.com>
2023-02-08 14:39:16 +0700

    Get films by title containing

--- src/main/java/com/filmsAPiDemo/filmsAPI/controllers/filmController.java
+++ src/main/java/com/filmsAPiDemo/filmsAPI/controllers/filmController.java
@@ -3,6 +3,7 @@ package com.filmsAPiDemo.filmsAPI.controllers;
 import com.filmsAPiDemo.filmsAPI.models.Film;
 import com.filmsAPiDemo.filmsAPI.models.ResponseObject;
 import com.filmsAPiDemo.filmsAPI.repositories.FilmRepository;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,24 +24,38 @@ public class filmController {
         return repository.findAll();
     }
 
-    //Get detail film by id
-    @GetMapping("/{id}")
-    ResponseEntity<ResponseObject> getFilmById(@PathVariable Long id){
-        Optional<Film> foundFilm = repository.findById(id);
+    //Get detail film by film_id
+    @GetMapping("/{film_id}")
+    ResponseEntity<ResponseObject> getFilmById(@PathVariable Long film_id){
+        Optional<Film> foundFilm = repository.findById(film_id);
         return foundFilm.isPresent() ?
                 ResponseEntity.status(HttpStatus.OK).body(
                         new ResponseObject("ok", "Querry film successfully", foundFilm)
                 ):
                 ResponseEntity.status(HttpStatus.NOT_FOUND).body(
-                        new ResponseObject("failed", "Cannot found film with id = " + id, "")
+                        new ResponseObject("failed", "Cannot found film with id = " + film_id, "")
                 );
+    }
+
+    //Get detail film by title
+    @GetMapping("/title/{titleContaining}")
+    ResponseEntity<ResponseObject> getFilmByTitle(@PathVariable @NotNull String titleContaining){
+        List<Film> foundFilms = repository.findByTitleContaining(titleContaining.trim());
+        if(foundFilms.size() > 0){
+            return ResponseEntity.status(HttpStatus.OK).body(
+                    new ResponseObject("ok", "Querry film successfully", foundFilms)
+            );
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                new ResponseObject("failed", "Cannot found film with title like '" + titleContaining + "'", "")
+        );
     }
 
     //Insert new film
     @PostMapping("/insert")
     ResponseEntity<ResponseObject> insertFilm(@RequestBody Film newFilm){
-        List<Film> foundFilm = repository.findBytitle(newFilm.getTitle().trim());
-        if(foundFilm.size() > 0){
+        List<Film> foundFilms = repository.findByTitle(newFilm.getTitle().trim());
+        if(foundFilms.size() > 0){
             return ResponseEntity.status(404).body(
                     new ResponseObject("failed","Film title already taken", "")
             );
--- src/main/java/com/filmsAPiDemo/filmsAPI/repositories/FilmRepository.java
+++ src/main/java/com/filmsAPiDemo/filmsAPI/repositories/FilmRepository.java
@@ -2,9 +2,16 @@ package com.filmsAPiDemo.filmsAPI.repositories;
 
 import com.filmsAPiDemo.filmsAPI.models.Film;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface FilmRepository extends JpaRepository<Film, Long> {
-    List<Film> findBytitle(String title);
+    List<Film> findByTitle(String title);
+
+    @Query(nativeQuery = true, value = "SELECT * from films where Title like %:partTitle%")
+    List<Film> findByTitleContainingTest(@Param("partTitle") String partTitle);
+
+    List<Film> findByTitleContaining(String titleContaining);
 }
