3146d732be0b54b4e5f61e5e7e00bb331cf35134
TrungPhamDac <84670001+TrungPhamDac@users.noreply.github.com>
2023-02-11 15:07:03 +0700

    Update, delete film

--- src/main/java/com/filmsAPiDemo/filmsAPI/controllers/filmController.java
+++ src/main/java/com/filmsAPiDemo/filmsAPI/controllers/filmController.java
@@ -25,7 +25,7 @@ public class filmController {
     }
 
     //Get detail film by film_id
-    @GetMapping("/{film_id}")
+    @GetMapping("/id/{film_id}")
     ResponseEntity<ResponseObject> getFilmById(@PathVariable Long film_id){
         Optional<Film> foundFilm = repository.findById(film_id);
         return foundFilm.isPresent() ?
@@ -62,6 +62,41 @@ public class filmController {
         }
         return ResponseEntity.status(HttpStatus.OK).body(
                 new ResponseObject("ok","Insert Film Successfully", repository.save(newFilm))
+        );
+    }
+
+    //Update, Upsert film
+    //If film has already existed => updated, else add new film
+    @PutMapping("/{film_id}")
+    ResponseEntity<ResponseObject> updateFilm(@RequestBody Film newFilm, @PathVariable Long film_id){
+        Film updatedFilm = repository.findById(film_id).map(film -> {
+            film.setFilm_id(newFilm.getFilm_id());
+            film.setTitle(newFilm.getTitle());
+            film.setLanguage(newFilm.getLanguage());
+            film.setPremiere(newFilm.getPremiere());
+            film.setRuntime(newFilm.getRuntime());
+            film.setImdb_Score(newFilm.getImdb_Score());
+            return repository.save(film);
+        }).orElseGet(() -> {
+            return repository.save(newFilm);
+        });
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("OK", "Updated Film Successfully", updatedFilm)
+        );
+    }
+
+    //Delete a film
+    @DeleteMapping("/{film_id}")
+    ResponseEntity<ResponseObject> deleteFilm(@PathVariable Long film_id){
+        boolean exist = repository.existsById(film_id);
+        if(exist) {
+            repository.deleteById(film_id);
+            return ResponseEntity.status(HttpStatus.OK).body(
+                    new ResponseObject("OK", "Deleted Film Successfully", "")
+            );
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                new ResponseObject("Failed", "Cannot find film to delete", "")
         );
     }
 }
